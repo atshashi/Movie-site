7a051c9a2497b6f60f0ba8e7e437ed222fb601b7
Shashi <Shashikant9993@gmail.com>
2023-03-12 14:26:48 +0530

    added API  and debug the code

--- script.js
+++ script.js
@@ -16,15 +16,43 @@ btn.addEventListener("click", function () {
 })
 
 
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b3b29d2079acd46344e9ab8ce42a455&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=6b3b29d2079acd46344e9ab8ce42a455&query=";
 
-let apikey = "6b3b29d2079acd46344e9ab8ce42a455";
-let url = "https://api.themoviedb.org/3/movie/550?api_key=6b3b29d2079acd46344e9ab8ce42a455"
 
-async function getData(url) {
-    const response = await fetch(url);
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
+returndata(APILINK); 
+async function returndata(url) {
+    // const response = await fetch(url);
+    // console.log(response);
+    // let data = await response.json();
+    // console.log(data.results);
+    fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            const res = data.results;
+            console.log(res);
+
+            res.forEach(r => {
+                
+                const outdiv = document.createElement("div");
+                const indiv = document.createElement("div");
+                const img = document.createElement("img");
+                const h3 = document.createElement("h3");
+
+                h3.innerText = `${r.title}`;
+                img.src = IMG_PATH + r.poster_path;
+
+                indiv.appendChild(img);
+                outdiv.appendChild(indiv);
+                outdiv.appendChild(h3);
+                bdy.appendChild(outdiv);
+                outdiv.setAttribute("id", "content");
+                indiv.setAttribute("class", "image");
+
+            })
+        })
 }
 
-getData(url);
